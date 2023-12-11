cdb5d49b5a0893725aa550743c587583989bb796
Hery Robles Coronado <rahe316426@gmail.com>
2023-12-11 00:36:22 -0700

    Changes

--- app.js
+++ app.js
@@ -63,4 +63,52 @@ function cargarAnimaciones(){
     })
 }
 
+//Codigo para mostrar msje en sweetAlert para contacto
+
+document.addEventListener('DOMContentLoaded', function () {
+    
+    const enviarBtn = document.querySelector('.contacto .btn');
+
+    enviarBtn.addEventListener('click', function (event) {
+        event.preventDefault(); 
+
+        const nombreInput = document.querySelector('input[placeholder="Nombre..."]');
+        const correoInput = document.querySelector('input[placeholder="Correo..."]');
+        const mensajeTextarea = document.querySelector('textarea[placeholder="Mensaje..."]');
+
+        if (nombreInput.value.trim() !== '' && correoInput.value.trim() !== '' && mensajeTextarea.value.trim() !== '') {
+            nombreInput.value = '';
+            correoInput.value = '';
+            mensajeTextarea.value = '';
+
+            Swal.fire({
+                title: 'Mensaje enviado',
+                text: '¡Gracias por tu mensaje, me pondré en contacto con usted muy pronto!',
+                icon: 'success',
+                confirmButtonText: 'Aceptar'
+            });
+        } else {
+            Swal.fire({
+                title: 'Error',
+                text: 'Por favor, llena todos los campos',
+                icon: 'error',
+                confirmButtonText: 'Aceptar'
+            });
+        }
+    });
+});
+
+//Codigo para descargar CV
+
+document.addEventListener('DOMContentLoaded', function () {
+    const descargarCVBtn = document.getElementById('descargarCV');
+
+    descargarCVBtn.addEventListener('click', function () {
+        const rutaPDF = 'docs/cv.pdf';
+
+        window.location.href = rutaPDF;
+    });
+});
+
+
 cargarAnimaciones();
